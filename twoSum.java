fab70fd1bcdee8b4e3fb54091b19057bafe039df
Jean Moura <jeanmoura@outlook.com>
2022-05-11 20:44:33 -0300

    Adding the question 1. Two Sum

---
+++ twoSum.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {   
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                if (target == nums[j] + nums[j-i]) {
+                    return new int[]{j-i, j};
+                }
+            }
+        }
+        return null;
+    }    
+}

aba152f0f4c85b5dec8b7ffbb9fe1fac3146c523
Jean Moura <jeanmoura@outlook.com>
2022-05-29 20:37:46 -0300

    Adding the Solution of the question 2235. Add Two Integers

---
+++ addTwoIntegers.java
@@ -0,0 +1,12 @@
+//Given two integers num1 and num2, return the sum of the two integers.
+
+//Space complexity O(1), time complexity O(1);
+
+public class Solution {
+    public int sum(int num1, int num2) {
+        // initalizing the variable
+        int result = 0;
+        // returning the sum of the two integers
+        return result = num1 + num2;
+        } 
+}

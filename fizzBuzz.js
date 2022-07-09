fdec92d44f0b7eaea3633983231c027dc53e6c4f
Jean Moura <jeanmoura@outlook.com>
2022-07-09 14:11:21 -0300

    Added solutions of question 412.FizzBuzz.

---
+++ fizzBuzz.js
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/fizz-buzz/
+
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+ var fizzBuzz = function(n) {
+    let list = [];
+    while (n !== 0) {
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 == 0 && i % 5 == 0)list.push("FizzBuzz");
+        else if (i % 3 == 0)list.push("Fizz");
+        else if(i % 5== 0)list.push("Buzz");
+        else list.push(""+i);
+    }
+    return list;
+    }
+};
